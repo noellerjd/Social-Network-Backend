a36a6baf0209eef0643e396093e7441bd14f287e
noellerjd <noellerninja@gmail.com>
2022-09-11 20:58:02 -0600

    Models and userControllers coded

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,36 @@
 const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reactions");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: new Date(),
+      get: (time) => time.toUTCString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return `${this.reactions.length}`;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
